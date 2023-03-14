dc4521621067b2a7328d64b202254aa686ee9841
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-14 19:47:25 -0300

    chore: add import signin

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { PsicologosRegister } from "../pages/signupPsicologo/SignupPsicologo";
 import { Home } from "../pages/home/Home";
 import PatientProfile from "../pages/patientProfile/PatientProfile";
+import { Signin } from "../pages/signin/Signin";
 
 export default function AppRoutes() {
   return (
