3b18ac5b67bce0b9f3e136b37e5d67bf2ddb58f1
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-14 19:35:17 -0300

    feat: add signin

---
+++ src/pages/signin/Signin.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import swal from "sweetalert";
+import {
+  SigninImg,
+  SigninButton,
+  SigninContainer,
+  SigninForm,
+  SigninInput,
+  TitleSignin,
+  SubTitleSignin,
+  AccountTitleSignin,
+} from "./styleSignin";
+import imgLogo from "../../assets/img/icon.png";
+import { useNavigate } from "react-router-dom";
+import { ISignin } from "../../interfaces/ISignin";
+import { signinService } from "../../service/authentcation/signinAuth";
+
+export const Signin = () => {
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    credential: "",
+    password: "",
+    type: "psicologo",
+  });
+
+  const handleChangesValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues((values: ISignin) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSignin = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const response: any = await signinService.Login(values);
+
+    if (!response) {
+      swal({
+        title: "Erro de login",
+        text: "Usuário e/ou senha inválidos",
+        icon: "error",
+        timer: 6000,
+      });
+    }
+
+    const jwt = response.data.token;
+    const type = response.data.type;
+    console.log(jwt);
+
+    if (jwt) {
+      localStorage.setItem("jwt", jwt);
+      localStorage.setItem("type", type);
+      swal({
+        title: "Bem vindo",
+        icon: "success",
+        timer: 3000,
+      });
+      navigate("/principal");
+    }
+  };
+
+  return (
+    <SigninContainer>
+      <SigninForm onSubmit={handleSignin}>
+        <SigninImg src={imgLogo} />
+        <TitleSignin>Acesse o Baddy Care</TitleSignin>
+        <SubTitleSignin>Informe seus dados e acesse sua conta:</SubTitleSignin>
+
+        <SigninInput
+          type="email"
+          name="credential"
+          id="email"
+          placeholder="Digite seu e-mail"
+          required
+          onChange={handleChangesValues}
+        />
+
+        <SigninInput
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Digite sua senha"
+          required
+          onChange={handleChangesValues}
+        />
+
+        <SigninButton type="submit">Entrar</SigninButton>
+        <SubTitleSignin>Não possui uma conta?</SubTitleSignin>
+        <AccountTitleSignin onClick={() => navigate("/signup")}>
+          Clique aqui!
+        </AccountTitleSignin>
+      </SigninForm>
+    </SigninContainer>
+  );
+};
---
+++ src/pages/signin/styleSignin.ts
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+export const SigninImg = styled.img`
+  width: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const SigninContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const TitleSignin = styled.h2`
+  text-align: center;
+  padding-top: 20px;
+  color: rgb(230, 230, 230);
+`;
+
+export const SubTitleSignin = styled.p`
+  text-align: center;
+  padding-top: 10px;
+  color: rgb(230, 230, 230);
+`;
+
+export const AccountTitleSignin = styled.p`
+  text-align: center;
+  padding-top: 10px;
+  color: rgb(230, 230, 230);
+  cursor: pointer;
+`;
+
+export const SigninForm = styled.form`
+  background: rgb(0, 0, 0);
+  background: linear-gradient(
+    90deg,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(77, 77, 77, 1) 19%,
+    rgba(165, 165, 166, 1) 50%,
+    rgba(77, 77, 77, 1) 78%,
+    rgba(0, 0, 0, 1) 100%
+  );
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+
+  @media (max-width: 720px) {
+    width: 80%;
+  }
+`;
+
+export const SigninInput = styled.input`
+  display: flex;
+  width: 80%;
+  height: 40px;
+  border: 1px solid rgb(150, 150, 150);
+  outline: none;
+  margin: 5px;
+  padding-left: 10px;
+  font-size: 14px;
+  box-shadow: 0 0 0px 1000px white inset;
+`;
+
+export const SigninButton = styled.button`
+  background-color: white;
+  border: none;
+  border-radius: 5px;
+  color: rgb(150, 150, 150);
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 10px;
+  margin: 5px;
+  width: 50%;
+  height: 40px;
+  cursor: pointer;
+  transition: all 0.8s ease-out;
+`;
