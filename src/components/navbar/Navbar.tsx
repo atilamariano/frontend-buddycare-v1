f9988dcde130083aab91afa526be64abcdc03dfd
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-03-14 20:04:16 -0300

    feat: add disconnect option, clearing token navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,21 @@
-
 import Navbar from 'react-bootstrap/Navbar';
-import { StyleNavbar, StyleLink, StyelMenu, StyleSpan } from './styleNavbar';
+import { StyleNavbar, StyleLink, StyelMenu, StyleSpan, StyleButton } from './styleNavbar';
 import { useState, useRef, useEffect } from 'react';
 import { HiHome } from 'react-icons/hi';
 import Icon from '../../assets/img/icon.png';
 
-
+// Define os itens de navegação da barra
 interface NavItem {
   label: string;
   url: string;
 }
-
 const navItems: NavItem[] = [
   { label: "Psicólogo", url: "/psicologos" },
   { label: "Paciente", url: "/login" },
   { label: "Admin", url: "/login" },
 ];
 
+// Função que renderiza os itens de navegação
 function renderNavItems(navItems: NavItem[], hideMenu: () => void) {
   return navItems.map((item) => (
     <li key={item.url}>
@@ -27,11 +26,25 @@ function renderNavItems(navItems: NavItem[], hideMenu: () => void) {
   ));
 }
 
-function NavBar() {
+function handleLogout() {
+  // aqui você pode adicionar a lógica para fazer o logout do usuário, como apagar o token do localStorage, por exemplo
+  localStorage.removeItem("token"); // remove o token do localStorage
+  setIsLoggedIn(false); // atualiza o estado isLoggedIn para false
+}
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const navRef = useRef<HTMLUListElement>(null);
+function setIsLoggedIn(isLoggedIn: boolean) {
+    // Armazena o valor de isLoggedIn no localStorage com a chave 'isLoggedIn'
+  localStorage.setItem('isLoggedIn', String(isLoggedIn));
+    // Define o estado de loggedIn como o valor de isLoggedIn
+  setIsLoggedIn(isLoggedIn);
+}
 
+
+function NavBar({ isUserLoggedIn }: { isUserLoggedIn: boolean }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Define estado do menu aberto/fechado
+  const navRef = useRef<HTMLUListElement>(null); // Referência para o elemento UL da navegação
+
+  // Efeito que adiciona event listener ao clicar no botão de menu para fechar o menu se clicar fora dele
   useEffect(() => {
     function handleOutsideClick(event: MouseEvent) {
       if (navRef.current && !navRef.current.contains(event.target as Node)) {
@@ -48,10 +61,12 @@ function NavBar() {
     };
   }, [isMenuOpen]);
 
+  // Função que alterna a visibilidade do menu
   function toggleMenu() {
     setIsMenuOpen((prev) => !prev);
   }
 
+  // Função que esconde o menu
   function hideMenu() {
     setIsMenuOpen(false);
   }
@@ -66,11 +81,40 @@ function NavBar() {
           </div>
           <StyleLink href="/" style={{ textAlign: "center", padding: "0 0 0 5rem " }}> <HiHome /> Home</StyleLink>
           <StyelMenu className="me-auto">
-            <text onClick={toggleMenu}>
-              <StyleSpan>Login</StyleSpan>
-            </text>
+            {/* Se o usuário estiver logado, exibe botões para configurações e logout */}
+            {/* Se o usuário não estiver logado, exibe um botão para login */}
+            {!isUserLoggedIn ?
+              <StyleButton onClick={toggleMenu}>
+                <StyleSpan>Login</StyleSpan>
+              </StyleButton>
+              :
+              <>
+                <text onClick={toggleMenu}>
+                  <StyleSpan>Configurações</StyleSpan>
+                </text>
+                <text onClick={handleLogout}>
+                  <StyleSpan>Logout</StyleSpan>
+                </text>
+              </>
+            }
             {isMenuOpen && (
-              <ul ref={navRef} style={{ position: 'absolute' }}>{renderNavItems(navItems, hideMenu)}</ul>
+              <ul ref={navRef} style={{ position: 'absolute' }}>
+                {isUserLoggedIn ? (
+                  <>
+                    <li>
+                      <StyleLink href="/">Perfil</StyleLink>
+                    </li>
+                    <li>
+                      <StyleLink href="/">Mensagens</StyleLink>
+                    </li>
+                    <li>
+                      <StyleLink href="/">Sair</StyleLink>
+                    </li>
+                  </>
+                ) : (
+                  renderNavItems(navItems, hideMenu)
+                )}
+              </ul>
             )}
           </StyelMenu>
         </StyleNavbar>
